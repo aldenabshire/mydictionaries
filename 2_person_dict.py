0b677663ef6cc262d77715dfcbd1c6f134f788cd
Alden <Alden@gmail.com>
2022-09-13 12:14:00 -0500

    dictionary class

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,21 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+#print(type(person['children']))
+
+#print(person['children'][1])
+
+#print(person['pets'])
+
+#print(person['pets']['cat'])
+
+for name in person['children']:
+
+    print(name)
+
+for pet in person['pets']:
+
+    print(person['pets'][pet])
--- 4_dictionary exercise start file.py
+++ 4_dictionary exercise start file.py
@@ -1,8 +1,10 @@
+import random
+
 # This program uses a dictionary as a deck of cards.
 
 def main():
     # Create a deck of cards.
-   
+    deck = create_deck()
 
     # Get the number of cards to deal.
     num_cards = int(input('How many cards should I deal? '))
@@ -10,6 +12,7 @@ def main():
 
 
     # Deal the cards.
+    deal_cards(deck, num_cards)
 
 
     
@@ -45,7 +48,7 @@ def create_deck():
             'Queen of Diamonds':10, 'King of Diamonds': 10}
 
     # Return the deck.
-
+    return deck
 
 
 
@@ -54,21 +57,24 @@ def create_deck():
 
 def deal_cards(deck, number):
     # Initialize an accumulator for the hand value.
-
+    handValue = 0
     
     
 
     # Make sure the number of cards to deal is not
     # greater than the number of cards in the deck.
-
+    if number > 52:
+        number = 52
     
     
 
     # Deal the cards and accumulate their values.
-    
+    for count in range(number):
+        card = random.choice(list(deck))
+        value = deck[card]
+        handValue += value
+        print(card)
 
-
-    
 
     # Display the value of the hand.
 
